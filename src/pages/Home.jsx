4e17bb50e055c15a7575ce7a314dca0e703f1c83
Neil Joseph <157965944+njbestil@users.noreply.github.com>
2024-01-29 01:14:36 +0400

    navbar
    add navbar and menu for pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Webpage from "../layouts/Webpage"
+
+export default function Home() {
+    return (
+        <>
+            <Webpage>
+                home
+            </Webpage>
+        </>
+    )
+}
